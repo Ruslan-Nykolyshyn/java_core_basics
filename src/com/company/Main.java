d90e51d36abc3f4a045a6e36068b4318faf3f7b0
Ruslan-Nykolyshyn <ruslan123nykolyschyn@gmail.com>
2021-02-11 17:43:30 +0200

    homwork1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Vvedit m- ");
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println("Vvedit n- ");
+        int n = sc.nextInt();
+        System.out.println("Vvedit k- ");
+        int k = sc.nextInt();
+        System.out.println("Vklad  dorinyuye");
+        sc.close();
+        for (int i=0; i<k;i++){
+            m += m + (m*n)/100;
+
+          System.out.println(m);}
+
     }
 }
